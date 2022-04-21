42aff317804a2143957eae3ccc871af15c6f4093
DavidMarMer <davidmarmer02@gmail.com>
2022-04-21 19:18:55 +0200

    Consolidated base

--- src/java/Database/DBManagement.java
+++ src/java/Database/DBManagement.java
@@ -1,18 +1,19 @@
 package PokeCircle.src.java.Database;
 
-/**
- * Authors: David Martínez Merencio and María León Pérez
- */
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MySQLConnection {
+/**
+ * Authors: David Martínez Merencio and María León Pérez
+ */
+
+public class DBManagement {
 
     private static Connection connection;
 
-    public static boolean createConnection(String user, String password, String url, String classForName){
+    /*Connects to database*/
+    public static boolean createConnection(String user, String password, String url, String classForName) {
         boolean correct = true;
         try {
             Class.forName(classForName);
@@ -22,16 +23,27 @@ public class MySQLConnection {
             System.err.println("Driver not found");
             correct = false;
         } catch (SQLException sqle) {
-            System.err.println("Database connection refused");
+            System.err.println("Database connection failed");
             correct = false;
         }
         return correct;
+    }
+
+    /*Disconnects from database*/
+    public void closeConnection() {
+        try {
+            connection.close();
+            System.out.println("Database connection closed");
+        } catch (SQLException sqle) {
+            System.err.println("Database disconnection failed");
+        }       
     }
 
     public static Connection getConexion() {
         return connection;
     }
 
+    /*Provisional*/
     public static void main(String[] args) {
         createConnection("root", "", "jdbc:mysql://localhost:3306/pokecircle", "com.mysql.cj.jdbc.Driver");
     }
