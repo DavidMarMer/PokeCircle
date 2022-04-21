19e1cba9ac1cb3ac746b8f823432dbe645914e9c
DavidMarMer <davidmarmer02@gmail.com>
2022-04-21 21:11:38 +0200

    BDManagement.java first writing

--- src/java/Database/DBManagement.java
+++ src/java/Database/DBManagement.java
@@ -2,7 +2,10 @@ package PokeCircle.src.java.Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  * Authors: David Martínez Merencio and María León Pérez
@@ -14,31 +17,142 @@ public class DBManagement {
 
     /*Connects to database*/
     public static boolean createConnection(String user, String password, String url, String classForName) {
-        boolean correct = true;
+        boolean correct = false;
         try {
             Class.forName(classForName);
             connection = (Connection) DriverManager.getConnection(url ,user, password);
             System.out.println("Database connection successful");
+            correct = true;
         } catch (ClassNotFoundException cnfe) {
             System.err.println("Driver not found");
-            correct = false;
         } catch (SQLException sqle) {
             System.err.println("Database connection failed");
-            correct = false;
         }
         return correct;
     }
 
     /*Disconnects from database*/
-    public void closeConnection() {
+    public static boolean closeConnection() {
+        boolean correct = true;
         try {
             connection.close();
             System.out.println("Database connection closed");
         } catch (SQLException sqle) {
             System.err.println("Database disconnection failed");
-        }       
+            correct =  false;
+        }
+        return correct;
     }
 
+    /*Inserts into the database a new pokemon*/
+    public static boolean insert(short number, String name, String type1, String type2, float weight, float height, String img) {
+        boolean correct = true;
+        try {
+            String command = "INSERT INTO pokecircle.pokemon VALUES ("
+                    + "" + number + ","
+                    + "'" + name + "',"
+                    + "'" + type1 + "',"
+                    + "'" + type2 + "',"
+                    + "" + weight + ","
+                    + "" + height + ","
+                    + "'" + img + "',"
+                    + ");";
+            Statement st = connection.createStatement();
+            st.executeUpdate(command);
+        } catch (SQLException sqle) {
+            System.err.println("Error inserting into the database");
+            correct = false;
+        }
+        return correct;
+    }
+
+    /*Returns only one row.
+    If and error happens, returns null. If no row is found, returns empty*/
+    public static String selectOne(short number) {
+        String pokemon = "";
+        String command = "SELECT * FROM pokecircle.pokemon WHERE number = " + number + ";";
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(command);
+            if (rs.next()) {
+                pokemon = rs.getShort("number") + ";"
+                    + rs.getString("name") + ";"
+                    + rs.getString("type1") + ";"
+                    + rs.getString("type2") + ";"
+                    + rs.getFloat("weight") + ";"
+                    + rs.getFloat("height") + ";"
+                    + rs.getString("img");
+            }
+        } catch (SQLException sqle) {
+            System.err.println("Unique select failed");
+            pokemon = null;
+        }
+        return pokemon;
+    }
+
+    /*Returns an ArrayList with the pokemons found applying a condition.
+    If no condition is applied, it returns all pokemons. If and error happens, returns null. If no row is found, returns empty*/
+    public static ArrayList<String> select(String condition) {
+        ArrayList<String> pokemons = new ArrayList<String>();
+        String pokemon = "";
+        try {
+            String command = "SELECT * FROM pokecircle.pokemon";
+            if (condition != null && !condition.isEmpty()) {
+                command += " WHERE " + condition;
+            }
+            command += ";";
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(command);
+            while (rs.next()) {
+                pokemon = rs.getShort("number") + ";"
+                    + rs.getString("name") + ";"
+                    + rs.getString("type1") + ";"
+                    + rs.getString("type2") + ";"
+                    + rs.getFloat("weight") + ";"
+                    + rs.getFloat("height") + ";"
+                    + rs.getString("img");
+                pokemons.add(pokemon);
+            }
+        } catch (SQLException sqle) {
+            System.err.println("General select failed");
+            pokemons = null;
+        }
+        return pokemons;
+    }
+
+    /*Deletes a pokemon*/
+    public boolean delete(short number){
+        boolean correct = true;
+        String command = "DELETE FROM pokecircle.pokemon WHERE number = " + number + ";";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(command);
+            System.out.println("Pokemon deleted");
+        } catch (SQLException ex) {
+            System.err.println("Error deleting the pokemon");
+            correct = false;
+        }
+        return correct;
+    }
+
+    /*Updates a pokemon*/
+    public boolean update(short number, String name, String type1, String type2, float weight, float height, String img) {
+        boolean correct = true;
+       String command = "UPDATE pokecircle.pokemon SET name = '" + name + "', type1 = '"
+                + type1 + "', type2 = '" + type2 + "', weight = " + weight + ", height = "
+                + height + ", img = '" + img + "' WHERE number = " + number + ";";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(command);
+            System.out.println("Pokemon updated");
+        } catch (SQLException sqle) {
+            System.err.println("Error updating the pokemon");
+            correct = false;
+        }
+        return correct;
+    }
+
+    /*Returns Connection object*/
     public static Connection getConexion() {
         return connection;
     }
